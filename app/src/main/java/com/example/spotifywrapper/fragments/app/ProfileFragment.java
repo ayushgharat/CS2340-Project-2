46449838e98492746d214b58d006e2c357715f60
Ayush Gharat <gharatayush27@gmail.com>
2024-03-28 19:48:57 -0400

    Change color of status bar and bottom navigation view

--- app/src/main/java/com/example/spotifywrapper/fragments/app/ProfileFragment.java
+++ app/src/main/java/com/example/spotifywrapper/fragments/app/ProfileFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,17 +15,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.spotifywrapper.HomeActivity;
 import com.example.spotifywrapper.MainActivity;
 import com.example.spotifywrapper.R;
+import com.example.spotifywrapper.utils.SharedViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * This fragment will show the user's profile details and enable them to
@@ -36,6 +43,8 @@ public class ProfileFragment extends Fragment {
     private Button bt_delete, bt_sign_out;
     private FirebaseAuth mAuth;
     private ImageButton bt_edit;
+    private ImageView iv_profile;
+    private SharedViewModel viewModel;
     private FirebaseFirestore db;
 
     public ProfileFragment() {
@@ -63,6 +72,24 @@ public class ProfileFragment extends Fragment {
         bt_delete = rootView.findViewById(R.id.bt_delete_account);
         bt_edit = rootView.findViewById(R.id.bt_edit_profile);
         bt_sign_out = rootView.findViewById(R.id.bt_sign_out);
+        iv_profile = rootView.findViewById(R.id.iv_profile_profile_picture);
+
+        viewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        // Observe changes in data from the viewModel class
+        viewModel.getUserJSON().observe(getViewLifecycleOwner(), userJSONData -> {
+            // Handle updated data
+            // This code will be executed when data changes
+            // Update UI or perform any other actions
+
+            try {
+                JSONObject userJSON = new JSONObject(userJSONData);
+                displayUserInfo(userJSON);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                showToast("Failed to parse user information");
+            }
+        });
 
         bt_delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,6 +140,35 @@ public class ProfileFragment extends Fragment {
         dialog.show();
     }
 
+    /**
+     * This function takes the updated information, stored as a JSON object, and adds the text to the
+     * components in the fragment. Since it is an asynchronous function, it has to run on the main UI thread
+     * as opposed to a background thread
+     * @param userJSON
+     * @throws JSONException
+     */
+    private void displayUserInfo(JSONObject userJSON) throws JSONException {
+
+        requireActivity().runOnUiThread(() -> {
+            try {
+
+                // Picasso is an external library that I am using to render the images into the imageview quickly
+                Picasso.get().load(userJSON.getJSONArray("images").getJSONObject(0).getString("url")).into(iv_profile);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Again, because of the asynchronous thing, I was using this function to render toasts
+     * @param message
+     */
+    private void showToast(String message) {
+        // Show Toast on the main thread
+        requireActivity().runOnUiThread(() -> Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show());
+    }
+
 
     private void deleteAccount() {
         String id = mAuth.getCurrentUser().getUid();
@@ -124,6 +180,8 @@ public class ProfileFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
+
+                            //This code removes the user from the database
                             db.collection("users").document(id)
                                     .delete()
                                     .addOnCompleteListener(new OnCompleteListener<Void>() {
